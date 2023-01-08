a030cf45897887a81b56cbcbb3c7a2ce7dbe6baf
Kevin <kevindetuno@gmail.com>
2023-01-08 16:25:20 +0100

    add a simple web application using django framework.

---
+++ learning_logs/urls.py
@@ -0,0 +1,16 @@
+"""Define URL patterns for learning_logs"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    # Page that shows all topics
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a sigle topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+]
